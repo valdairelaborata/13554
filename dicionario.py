e526d4641d61e26ee4d65a60c49700280e1dfb28
Valdair <valdairelaborata@gmail.com>
2026-08-05 19:11:18 -0300

    dicionario

--- dicionario.py
+++ dicionario.py
@@ -2,8 +2,9 @@
 
 clientes = {"nome": "Batman", 
             "telefone": 41253696585,
-            "email": "dicionario@gmail.com",
-            "Endereco": "Batcaverna"}
+            "email": "dicionario@gmail.com"
+            }
+            
 
 # print(clientes["nome"])
 
@@ -13,9 +14,15 @@ clientes = {"nome": "Batman",
 # clientes["Endereco"] = "Batcaverna"
 
 
-clientes["telefone"] = [41253696585, 41952365258]
+# clientes["telefone"] = [41253696585, 41952365258]
 
-del clientes["Endereco"]
+# del clientes["Endereco"]
 
-print(clientes)
-print(clientes.pop("Endereco","Chave não encontrada!"))
+# print(clientes)
+# print(clientes.pop("Endereco","Chave não encontrada!"))
+
+# endereco = {"Endereco": "Batcaverna"}
+
+# clientes.update(endereco)
+
+# print(clientes)
