39ac4613bf7f51446d76e2e8eda1d6031a13ef87
Valdair <valdairelaborata@gmail.com>
2026-08-04 21:32:10 -0300

    dicionario

--- dicionario.py
+++ dicionario.py
@@ -2,16 +2,20 @@
 
 clientes = {"nome": "Batman", 
             "telefone": 41253696585,
-            "email": "dicionario@gmail.com"}
+            "email": "dicionario@gmail.com",
+            "Endereco": "Batcaverna"}
 
 # print(clientes["nome"])
 
 # nome = clientes.get("endereço","Valor não encontrado!")
 # print(nome)
 
-clientes["Endereco"] = "Batcaverna"
+# clientes["Endereco"] = "Batcaverna"
 
 
 clientes["telefone"] = [41253696585, 41952365258]
 
+del clientes["Endereco"]
+
 print(clientes)
+print(clientes.pop("Endereco","Chave não encontrada!"))
