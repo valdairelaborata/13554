caec7c276fb2c304f16b02e2a4c737fd4dcfdfa1
Valdair <valdairelaborata@gmail.com>
2026-08-05 21:13:17 -0300

    lab_01

--- lab_01.py
+++ lab_01.py
@@ -1,1 +1,10 @@
 # 1) Faça um programa que receba uma letra e verifique se esta é vogal ou consoante.
+
+letra = input("A: ").lower()
+
+vogais = ["a", "e", "i", "o", "u"]
+ 
+if letra in vogais:
+    print("É uma vogal")
+else:
+    print("É uma consoante")
