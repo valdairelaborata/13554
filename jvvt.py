3cbf3d9e0a5b5762ce20cee119d27ba62bd83406
Valdair <valdairelaborata@gmail.com>
2026-08-14 19:49:07 -0300

    lab

--- jvvt.py
+++ jvvt.py
@@ -1,9 +1,8 @@
-arquivo = open("ips.txt", "r")
-conteudo = arquivo.read()
-arquivo.close()
- 
- 
-arquivo = open("ip.txt", "w")
-arquivo.write("[Endereços validos:]\n 200.135.80.9\n 192.168.1.1\n 8.35.67.74\n 257.32.4.5\n")
-arquivo.write("\n[Endereços invalidos:]\n 85.345.1.2\n 1.2.3.4\n 9.8.234.5\n 192.168.0.256\n")
-arquivo.close()
+import os
+os.makedirs("diretorio")
+
+os.chdir("diretorio")
+
+open("arquivo_01.txt", "w")
+open("arquivo_02.txt", "w")
+open("arquivo_03.txt", "w")
--- lab.py
+++ lab.py
@@ -1,1 +1,16 @@
 # Crie um diretório e nele crie 3 arquivos .txt. 
+
+
+import os
+os.makedirs("diretorio")
+
+os.chdir("diretorio")
+
+arquivo_01 = open("arquivo_01.txt", "w")
+arquivo_01.close()
+
+arquivo_02 = open("arquivo_02.txt", "w")
+arquivo_02.close()
+
+arquivo_03 = open("arquivo_03.txt", "w")
+arquivo_03.close()
